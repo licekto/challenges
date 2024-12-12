43ebe45b605f1190017ac524591c23d972aecd73
Tomas Licek <licekto@gmail.com>
2024-12-12 12:50:06 +0100

    Day 12, part 2

--- advent-of-code/2024/python/advent_of_code/day_12.py
+++ advent-of-code/2024/python/advent_of_code/day_12.py
@@ -1,4 +1,9 @@
 
+
+def is_on_map(m, x, y):
+    return y in range(len(m)) and x in range(len(m[0]))
+
+
 def fill_and_calc(m, visited, x, y):
 
     if (x, y) in visited:
@@ -10,15 +15,31 @@ def fill_and_calc(m, visited, x, y):
     area = 0
     for sx, sy in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
         nx, ny = x + sx, y + sy
-        is_on_map = ny in range(len(m)) and nx in range(len(m[0]))
-        if not is_on_map or m[ny][nx] != current:
+        if not is_on_map(m, nx, ny) or m[ny][nx] != current:
             perimeter += 1
-        elif is_on_map and m[ny][nx] == current and (nx, ny) not in visited:
+        elif is_on_map(m, nx, ny) and m[ny][nx] == current and (nx, ny) not in visited:
             sub_perimeter, sub_area = fill_and_calc(m, visited, nx, ny)
             perimeter += sub_perimeter
             area += sub_area
 
     return perimeter, area + 1
+
+
+def get_sides(area):
+    s = 0
+
+    for x, y in area:
+        s += (x - 1, y) not in area and (x, y - 1) not in area
+        s += (x + 1, y) not in area and (x, y - 1) not in area
+        s += (x - 1, y) not in area and (x, y + 1) not in area
+        s += (x + 1, y) not in area and (x, y + 1) not in area
+
+        s += (x - 1, y) in area and (x, y - 1) in area and (x - 1, y - 1) not in area
+        s += (x + 1, y) in area and (x, y - 1) in area and (x + 1, y - 1) not in area
+        s += (x - 1, y) in area and (x, y + 1) in area and (x - 1, y + 1) not in area
+        s += (x + 1, y) in area and (x, y + 1) in area and (x + 1, y + 1) not in area
+
+    return s
 
 
 def solve(lines):
@@ -33,23 +54,11 @@ def solve(lines):
         for x in range(len(m[0])):
             if (x, y) in visited:
                 continue
-            perimeter, area = fill_and_calc(m, visited, x, y)
-            res1 += perimeter * area
+            current_visited = set()
+            perimeter, area = fill_and_calc(m, current_visited, x, y)
+            sides = get_sides(current_visited)
+            res1 += area * perimeter
+            res2 += area * sides
+            visited.update(current_visited)
 
     return res1, res2
-
-
-lines = [
-    'RRRRIICCFF',
-    'RRRRIICCCF',
-    'VVRRRCCFFF',
-    'VVRCCCJFFF',
-    'VVVVCJJCFE',
-    'VVIVCCJJEE',
-    'VVIIICJJEE',
-    'MIIIIIJJEE',
-    'MIIISIJEEE',
-    'MMMISSJEEE'
-]
-
-solve(lines)
--- advent-of-code/2024/python/test/test_12.py
+++ advent-of-code/2024/python/test/test_12.py
@@ -3,8 +3,8 @@ from advent_of_code.day_12 import *
 
 
 @pytest.mark.parametrize('file_path, expected_part1, expected_part2',
-                     [(['12', 'ref.in'], 1930, 0),
-                      (['12', 'user.in'], 1450422, 0)
+                     [(['12', 'ref.in'], 1930, 1206),
+                      (['12', 'user.in'], 1450422, 906606)
                      ])
 def test_02(get_data, file_path, expected_part1, expected_part2):
     assert(solve(get_data(file_path)) == (expected_part1, expected_part2))
