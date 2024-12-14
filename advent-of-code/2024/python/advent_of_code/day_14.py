ea6df9229e4e3a1131dd674f3975a0a5dca7dbc4
Tomas Licek <licekto@gmail.com>
2024-12-14 12:35:32 +0100

    Day 14 part 2

--- advent-of-code/2024/python/advent_of_code/day_14.py
+++ advent-of-code/2024/python/advent_of_code/day_14.py
@@ -14,39 +14,10 @@ def print_robots(robots, w, h):
                     c += 1
             print(f'{c if c != 0 else "."}', end='')
         print()
-
-
-#def is_christmas_tree(robots, w, h):
+    print()
 
 
 def calc_quadrants(robots, w, h):
-    pass
-
-
-def solve(lines, w, h):
-
-    print()
-    res1 = 0
-    res2 = 0
-
-    p = re.compile('p=([0-9]+),([0-9]+) v=(-?[0-9]+),(-?[0-9]+)')
-
-    robots = []
-    for line in lines:
-        m = p.findall(line)
-        robots.append(list(map(int, m[0])))
-
-    #print(robots)
-    SEC = 100
-
-    for s in range(SEC):
-        #print_robots(robots, w, h)
-        for r in range(len(robots)):
-            x, y, vx, vy = robots[r]
-            robots[r][0] = wrap(x + vx, w)
-            robots[r][1] = wrap(y + vy, h)
-
-
     q1x, q1y = range(int(w / 2)), range(int(h / 2))
     q2x, q2y = range(int(w / 2) + 1, w), range(int(h / 2))
     q3x, q3y = range(int(w / 2)), range(int(h / 2) + 1, h)
@@ -59,6 +30,65 @@ def solve(lines, w, h):
         q3c += 1 if x in q3x and y in q3y else 0
         q4c += 1 if x in q4x and y in q4y else 0
 
-    res1 = q1c * q2c * q3c * q4c
+    return q1c * q2c * q3c * q4c
+
+
+def dfs(x, y, graph, w, h, visited):
+    if x not in range(w) or y not in range(h) or (x, y) in visited or (x, y) not in graph:
+        return 0
+
+    visited.add((x, y))
+    comp = 1
+    dirs = [(0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]
+    for sx, sy in dirs:
+        comp += dfs(x + sx, y + sy, graph, w, h, visited)
+
+    return comp
+
+
+def connected(robots, w, h):
+    graph = set()
+    for x, y, _, _ in robots:
+        graph.add((x, y))
+
+    visited = set()
+    max_comp = 0
+
+    for x, y, _, _ in robots:
+        max_comp = max(max_comp, dfs(x, y, graph, w, h, visited))
+
+    return max_comp
+
+
+def solve(lines, w, h):
+    res1 = 0
+    res2 = 0
+
+    p = re.compile('p=([0-9]+),([0-9]+) v=(-?[0-9]+),(-?[0-9]+)')
+
+    robots = []
+    for line in lines:
+        m = p.findall(line)
+        robots.append(list(map(int, m[0])))
+
+    SEC = 100
+
+    max_comp = 0
+    for s in range(1, 10000):
+        for r in range(len(robots)):
+            x, y, vx, vy = robots[r]
+            robots[r][0] = wrap(x + vx, w)
+            robots[r][1] = wrap(y + vy, h)
+
+        if s == SEC:
+            res1 = calc_quadrants(robots, w, h)
+
+        comp = connected(robots, w, h)
+        if comp > max_comp:
+            max_comp = comp
+            res2 = s
+            #print(f'sec={s}, comp={comp}')
+            #print_robots(robots, w, h)
+
 
     return res1, res2
--- advent-of-code/2024/python/test/test_14.py
+++ advent-of-code/2024/python/test/test_14.py
@@ -3,8 +3,8 @@ from advent_of_code.day_14 import *
 
 
 @pytest.mark.parametrize('file_path, expected_part1, expected_part2, w, h',
-                     [(['14', 'ref.in'], 12, 0, 11, 7),
-#                      (['14', 'user.in'], 221655456, 0, 101, 103)
+                     [(['14', 'ref.in'], 12, 45, 11, 7), # dummy result for part 2
+                      (['14', 'user.in'], 221655456, 7858, 101, 103)
                      ])
 def test_02(get_data, file_path, expected_part1, expected_part2, w, h):
     assert(solve(get_data(file_path), w, h) == (expected_part1, expected_part2))
